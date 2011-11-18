fd35864fbd5b42235be98de52db8e727e97b5434
Haseeb Khan <haseeb@tkxel.com>
2011-11-18 11:14:43 +0500

    New Rails App generated using rails new haseeb-rails-demos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HaseebRailsDemo::Application.config.secret_token = '17fefd22b9f5f1d6bd3552b2cac7fb85e05792369d671d85463759833255c45d85989c69c12cb2c61a2b4d72a4fe88bf819d308d4a6aac8eb0c11187bf103361'
